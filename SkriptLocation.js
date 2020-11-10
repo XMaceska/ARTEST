586110b83a67c93befdcf428e3720d92b7d2df9b
XMaceska <dave.maceska@seznam.cz>
2020-11-10 12:45:17 +0100

    zkouska primitives

--- SkriptLocation.js
+++ SkriptLocation.js
@@ -8,8 +8,8 @@ function staticLoadPlaces() {
         {
             name: 'church',
             location: {
-                lat: 50.0752706,
-                lng: 14.4976731,
+                lat: 50.0756456,
+                lng: 14.4979708,
             }
         },
     ];
@@ -25,7 +25,7 @@ function renderPlaces(places) {
         let model = document.createElement('a-entity');
         model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
         model.setAttribute('gltf-model','./primitives/primitives.gltf');
-        model.setAttribute('rotation', '0 180 0');
+        model.setAttribute('rotation', '0 0 0');
         model.setAttribute('animation-mixer', '');
         model.setAttribute('scale', '120 120 120');
 
