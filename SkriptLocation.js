c51bc2c5c7a159827977bfdda8e009163a1aad36
XMaceska <dave.maceska@seznam.cz>
2020-11-10 12:03:54 +0100

    zkouska obj

--- SkriptLocation.js
+++ SkriptLocation.js
@@ -24,7 +24,8 @@ function renderPlaces(places) {
 
         let model = document.createElement('a-entity');
         model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-        model.setAttribute('gltf-model', './church/church2.gltf');
+        model.setAttribute('obj-model', 'obj', './church/church-obj.obj');
+        model.setAttribute('obj-model', 'mtl', './church/church-obj.mtl');
         model.setAttribute('rotation', '0 180 0');
         model.setAttribute('animation-mixer', '');
         model.setAttribute('scale', '120 120 120');
