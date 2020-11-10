d169ea08cb8ea9edb392d31a237125df8904018b
XMaceska <dave.maceska@seznam.cz>
2020-11-10 11:32:58 +0100

    test z blenderu

--- SkriptLocation.js
+++ SkriptLocation.js
@@ -24,7 +24,7 @@ function renderPlaces(places) {
 
         let model = document.createElement('a-entity');
         model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-        model.setAttribute('gltf-model', './church/church.gltf');
+        model.setAttribute('gltf-model', './church/church2.gltf');
         model.setAttribute('rotation', '0 180 0');
         model.setAttribute('animation-mixer', '');
         model.setAttribute('scale', '120 120 120');
