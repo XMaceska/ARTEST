ac8f9d5a95b25ff727eee15adaec88d218c1673f
XMaceska <dave.maceska@seznam.cz>
2020-11-10 12:29:58 +0100

    test primitives

--- SkriptLocation.js
+++ SkriptLocation.js
@@ -24,8 +24,7 @@ function renderPlaces(places) {
 
         let model = document.createElement('a-entity');
         model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-        model.setAttribute('obj-model', 'obj', './church/church-obj.obj');
-        model.setAttribute('obj-model', 'mtl', './church/church-obj.mtl');
+        model.setAttribute('gltf-model','./primitives/primitives.gltf');
         model.setAttribute('rotation', '0 180 0');
         model.setAttribute('animation-mixer', '');
         model.setAttribute('scale', '120 120 120');
